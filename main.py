76e37add15a27e28275e9afe015877cacdba100c
J Garza <garzajnc14@gmail.com>
2025-02-28 11:28:56 -0500

    Updated run.py, conda.yml and main.py

--- main.py
+++ main.py
@@ -1,108 +1,126 @@
-import json
-
-import mlflow
-import tempfile
 import os
-import wandb
 import hydra
 from omegaconf import DictConfig
+import mlflow
 
-_steps = [
-    "download",
-    "basic_cleaning",
-    "data_check",
-    "data_split",
-    "train_random_forest",
-    # NOTE: We do not include this in the steps so it is not run by mistake.
-    # You first need to promote a model export to "prod" before you can run this,
-    # then you need to run this step explicitly
-#    "test_regression_model"
-]
-
-
-# This automatically reads in the configuration
-@hydra.main(config_name='config')
+@hydra.main(config_path=".", config_name="config")
 def go(config: DictConfig):
+    # Split the steps defined in the config
+    active_steps = config["main"]["steps"].split(",")
 
-    # Setup the wandb experiment. All runs will be grouped under this name
-    os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
-    os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
+    # -----------------------------
+    # Step: Download Data
+    # -----------------------------
+    if "download" in active_steps:
+        _ = mlflow.run(
+            f"{config['main']['components_repository']}/get_data",
+            "main",
+            version="main",
+            env_manager="conda",
+            parameters={
+                "sample": config["etl"]["sample"],
+                "artifact_name": "sample.csv",
+                "artifact_type": "raw_data",
+                "artifact_description": "Raw file as downloaded"
+            },
+        )
 
-    # Steps to execute
-    steps_par = config['main']['steps']
-    active_steps = steps_par.split(",") if steps_par != "all" else _steps
+    # -----------------------------
+    # Step: Exploratory Data Analysis (EDA)
+    # -----------------------------
+    if "eda" in active_steps:
+        _ = mlflow.run(
+            os.path.join(hydra.utils.get_original_cwd(), "src", "eda"),
+            "main",
+            env_manager="conda",
+            parameters={},
+        )
 
-    # Move to a temporary directory
-    with tempfile.TemporaryDirectory() as tmp_dir:
+    # -----------------------------
+    # Step: Basic Cleaning
+    # -----------------------------
+    if "basic_cleaning" in active_steps:
+        _ = mlflow.run(
+            os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+            "main",
+            env_manager="conda",
+            parameters={
+                "input_artifact": "sample.csv:latest",
+                "output_artifact": "clean_sample.csv",
+                "output_type": "clean_sample",
+                "output_description": "Data with outliers and null values removed",
+                "min_price": config["etl"]["min_price"],
+                "max_price": config["etl"]["max_price"]
+            },
+        )
 
-        if "download" in active_steps:
-            # Download file and load in W&B
-            _ = mlflow.run(
-                f"{config['main']['components_repository']}/get_data",
-                "main",
-                version='main',
-                env_manager="conda",
-                parameters={
-                    "sample": config["etl"]["sample"],
-                    "artifact_name": "sample.csv",
-                    "artifact_type": "raw_data",
-                    "artifact_description": "Raw file as downloaded"
-                },
-            )
+    # -----------------------------
+    # Step: Data Check
+    # -----------------------------
+    if "data_check" in active_steps:
+        _ = mlflow.run(
+            os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+            "main",
+            env_manager="conda",
+            parameters={
+                "csv": "clean_sample.csv:latest",
+                "ref": "clean_sample.csv:reference",
+                "kl_threshold": config["data_check"]["kl_threshold"],
+                "min_price": config["etl"]["min_price"],
+                "max_price": config["etl"]["max_price"]
+            },
+        )
 
-        if "basic_cleaning" in _steps:
-            _ = mlflow.run(
-                f"{config['main']['components_repository']}/basic_cleaning",
-                "main",
-                version='main',
-                env_manager="conda",
-                parameters={
-                    "input_artifact": "sample.csv:latest",
-                    "output_artifact": "clean_sample.csv",
-                    "output_type": "dataset",
-                    "output_description": "Cleaned Airbnb dataset",
-                    "min_price": config["etl"]["min_price"],
-                    "max_price": config["etl"]["max_price"]
-                }
-            )
+    # -----------------------------
+    # Step: Data Split
+    # -----------------------------
+    if "data_split" in active_steps:
+        _ = mlflow.run(
+            f"{config['main']['components_repository']}/train_val_test_split",
+            "main",
+            version="main",
+            env_manager="conda",
+            parameters={
+                "input": "clean_sample.csv:latest",
+                "test_size": "0.10",
+                "random_seed": "42",
+                "stratify_by": "none"
+            },
+        )
 
+    # -----------------------------
+    # Step: Train Random Forest
+    # -----------------------------
+    if "train_random_forest" in active_steps:
+        _ = mlflow.run(
+            os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+            "main",
+            env_manager="conda",
+            parameters={
+                "trainval_artifact": "trainval_data.csv:latest",
+                "val_size": "0.10",
+                "random_seed": "42",
+                "stratify_by": "neighbourhood_group",
+                "rf_config": config["modeling"]["random_forest"],
+                "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                "output_artifact": "random_forest_export"
+            },
+        )
 
-        if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
-
-        if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
-
-        if "train_random_forest" in active_steps:
-
-            # NOTE: we need to serialize the random forest configuration into JSON
-            rf_config = os.path.abspath("rf_config.json")
-            with open(rf_config, "w+") as fp:
-                json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
-
-            # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
-            # step
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
-
-        if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
-
+    # -----------------------------
+    # Step: Test Regression Model
+    # -----------------------------
+    if "test_regression_model" in active_steps:
+        _ = mlflow.run(
+            os.path.join(hydra.utils.get_original_cwd(), "src", "test_regression_model"),
+            "main",
+            env_manager="conda",
+            parameters={
+                "input_artifact": "random_forest_export:latest",
+                "mlflow_model": "random_forest_model",
+                "output_artifact": "regression_model_report"
+            },
+        )
 
 if __name__ == "__main__":
     go()
--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -1,63 +1,86 @@
 #!/usr/bin/env python
 """
-Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
+Download from W&B the raw dataset and apply some basic data cleaning,
+exporting the result to a new artifact.
 """
 import argparse
 import logging
 import wandb
 import pandas as pd
 
-
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
-# DO NOT MODIFY
 def go(args):
-
     run = wandb.init(job_type="basic_cleaning")
     run.config.update(args)
-
-    # Download input artifact. This will also log that this script is using this
     
-    run = wandb.init(project="nyc_airbnb", group="cleaning", save_code=True)
+    # Download input artifact from W&B
     artifact_local_path = run.use_artifact(args.input_artifact).file()
     df = pd.read_csv(artifact_local_path)
-    # Drop outliers
-    min_price = args.min_price
-    max_price = args.max_price
-    idx = df['price'].between(min_price, max_price)
+    logging.info(f'The file "{artifact_local_path}" was successfully read.')
+    
+    # Filter the data: keep rows where price is between min_price and max_price
+    idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
-    # Convert last_review to datetime
+    
+    # Convert last_review to datetime format
     df['last_review'] = pd.to_datetime(df['last_review'])
-
-    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
-    df = df[idx].copy()
-    # Save the cleaned file
-    df.to_csv('clean_sample.csv',index=False)
-
-    # log the new data.
+    
+    # Save the cleaned data to a new CSV file
+    clean_sample_file = "clean_sample.csv"
+    df.to_csv(clean_sample_file, index=False)
+    
+    # Create a new artifact for the cleaned data and log it to W&B
     artifact = wandb.Artifact(
-     args.output_artifact,
-     type=args.output_type,
-     description=args.output_description,
- )
-    artifact.add_file("clean_sample.csv")
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description
+    )
+    artifact.add_file(clean_sample_file)
     run.log_artifact(artifact)
+    logging.info(f'The file "{clean_sample_file}" was successfully uploaded to the W&B server.')
+    run.finish()
 
-
-# TODO: In the code below, fill in the data type for each argumemt. The data type should be str, float or int. 
-# TODO: In the code below, fill in a description for each argument. The description should be a string.
 if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description="Perform basic data cleaning")
-
-    parser.add_argument("--input_artifact", type=str, required=True, help="Input dataset artifact from W&B")
-    parser.add_argument("--output_artifact", type=str, required=True, help="Name of the output cleaned dataset artifact")
-    parser.add_argument("--output_type", type=str, required=True, help="Type of the output artifact")
-    parser.add_argument("--output_description", type=str, required=True, help="Description of the output artifact")
-    parser.add_argument("--min_price", type=float, required=True, help="Minimum valid price")
-    parser.add_argument("--max_price", type=float, required=True, help="Maximum valid price")
-
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
+    
+    parser.add_argument(
+        "--input_artifact", 
+        type=str,
+        help="The input artifact",
+        required=True
+    )
+    parser.add_argument(
+        "--output_artifact", 
+        type=str,
+        help="The name for the output artifact",
+        required=True
+    )
+    parser.add_argument(
+        "--output_type", 
+        type=str,
+        help="The type for the output artifact",
+        required=True
+    )
+    parser.add_argument(
+        "--output_description", 
+        type=str,
+        help="A description for the output artifact",
+        required=True
+    )
+    parser.add_argument(
+        "--min_price", 
+        type=float,
+        help="The minimum price to consider",
+        required=True
+    )
+    parser.add_argument(
+        "--max_price", 
+        type=float,
+        help="The maximum price to consider",
+        required=True
+    )
+    
     args = parser.parse_args()
-
     go(args)
