5e51ae58b3f0065c6cacd95a2c8956e72bc95b4f
garzanc24 <garzajnc14@gmail.com>
2025-03-08 11:21:38 -0500

    Fix basic_cleaning step in ML pipeline
    - Updated main.py to properly reference local src/basic_cleaning directory
    - Fixed parameter passing by replacing spaces with underscores in description
    - Successfully implemented data cleaning step that filters price outliers and invalid locations

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import json
-
 import mlflow
 import tempfile
 import os
@@ -13,30 +12,25 @@ _steps = [
     "data_check",
     "data_split",
     "train_random_forest",
-    # NOTE: We do not include this in the steps so it is not run by mistake.
-    # You first need to promote a model export to "prod" before you can run this,
-    # then you need to run this step explicitly
-#    "test_regression_model"
+    # "test_regression_model"
 ]
 
-
-# This automatically reads in the configuration
-@hydra.main(config_name='config')
+@hydra.main(config_path=".", config_name='config')
 def go(config: DictConfig):
-
-    # Setup the wandb experiment. All runs will be grouped under this name
+    # Setup the wandb experiment
     os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
     os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
 
-    # Steps to execute
+    # Determine steps to execute
     steps_par = config['main']['steps']
     active_steps = steps_par.split(",") if steps_par != "all" else _steps
 
-    # Move to a temporary directory
+    # Use a temporary directory
     with tempfile.TemporaryDirectory() as tmp_dir:
+        os.chdir(tmp_dir)
 
         if "download" in active_steps:
-            # Download file and load in W&B
+            # Download file using the remote repository
             _ = mlflow.run(
                 f"{config['main']['components_repository']}/get_data",
                 "main",
@@ -51,56 +45,39 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
+            # Define the root path
+            root_path = hydra.utils.get_original_cwd()
+            
+            # Run the basic_cleaning step
             _ = mlflow.run(
-                f"{config['main']['components_repository']}/basic_cleaning",
+                os.path.join(root_path, "src", "basic_cleaning"),
                 "main",
-                version='main',
-                env_manager="conda",
                 parameters={
-                    "input_artifact": "sample.csv",
+                    "input_artifact": "sample.csv:latest",
                     "output_artifact": "clean_sample.csv",
-                    "output_type": "clean_data",
-                    "min_price": config["etl"]["min_price"],
-                    "max_price": config["etl"]["max_price"]
+                    "output_type": "clean_sample",
+                    "output_description": "Data_with_outliers_and_null_values_removed",  # Use underscores instead of spaces
+                    "min_price": float(config["etl"]["min_price"]),
+                    "max_price": float(config["etl"]["max_price"])
                 },
             )
 
+        # Placeholder for other steps
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
             pass
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
             pass
 
         if "train_random_forest" in active_steps:
-
-            # NOTE: we need to serialize the random forest configuration into JSON
-            rf_config = os.path.abspath("rf_config.json")
+            # Serialize random forest configuration
+            rf_config = os.path.join(tmp_dir, "rf_config.json")
             with open(rf_config, "w+") as fp:
-                json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
-
-            # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
-            # step
-
-            ##################
-            # Implement here #
-            ##################
-
+                json.dump(dict(config["modeling"]["random_forest"].items()), fp)
             pass
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
             pass
-
 
 if __name__ == "__main__":
     go()
